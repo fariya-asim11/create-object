11404422ab177243dd5a7668fdc6cc57b68ce027
fariya <fariyaasim341484@gmail.com>
2024-10-01 00:26:52 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+var person1 = {
+firstName: "John",
+lastName: "Doe",
+age: 50,
+eyeColor: "blue"
+};
+
+document.getElementById("div1").innerHTML = person1.firstName + "is" + person1.age + "years old.";
+
+var person2 = {
+firstName: "Emily",
+lastName: "Smith",
+age: 30,
+eyeColor: "green"
+};
+
+document.getElementById("div2").innerHTML = person2.firstName + "is" + person2.age + "years old.";
+    
+var person3 = {
+firstName: "Michael",
+lastName: "Johnson",
+age: 40,
+eyeColor: "brown"
+};
+
+document.getElementById("div3").innerHTML = person3.firstName + "is" + person3.age + "years old.";
+
+var person4 = {
+firstName: "Sarah",
+lastName: "Lee",
+age: 25,
+eyeColor: "hazel"
+};
+
+document.getElementById("div4").innerHTML = person4.firstName + "is" + person4.age + "years old.";
+        
+var person5 = {
+firstName: "William",
+lastName: "Davis",
+age: 60,
+eyeColor: "gray"
+};
+
+document.getElementById("div5").innerHTML = person5.firstName + "is" + person5.age + "years old.";
+
+var person6 = {
+firstName: "Olivia",
+lastName: "Martin",
+age: 35,
+eyeColor: "amber"
+};
+
+document.getElementById("div6").innerHTML = person6.firstName + "is" + person6.age + "years old.";
+
+var person7 = {
+firstName: "James",
+lastName: "Wilson",
+age: 45,
+eyeColor: "blue-green"
+};
+
+document.getElementById("div7").innerHTML = person7.firstName + "is" + person7.age + "years old.";
+
+var person8 = {
+firstName: "Ava",
+lastName: "Garcia",
+age: 20,
+eyeColor: "dark brown"
+};
+
+document.getElementById("div8").innerHTML = person8.firstName + "is" + person8.age + "years old.";
+            
+var person9 = {
+firstName: "Robert",
+lastName: "Harris",
+age: 55,
+eyeColor: "light blue"
+};
+
+document.getElementById("div9").innerHTML = person9.firstName + "is" + person9.age + "years old.";
+            
+ var person10 = {
+firstName: "Isabella",
+lastName: "Miller",
+age: 28,
+eyeColor: "turquoise"
+};
+
+document.getElementById("div10").innerHTML = person10.firstName + "is" + person10.age + "years old.";
